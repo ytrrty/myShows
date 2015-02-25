7bc08b30ab5cf441191fbfe7df204d7cd2fa1ae0
ytrrty <ytrrty@bk.ru>
2015-02-25 14:32:41 +0200

    Admin control panel

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
 
+  mount RailsAdmin::Engine => '/admin', as: 'rails_admin'
   devise_for :users, controllers: { registrations: 'registrations' }
 
   resources :users
