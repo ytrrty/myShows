9be8ffb4c7b90af0dc3967c2d54e5fc655dbf1f0
Dmytro Matsiuk <dmmmat@gmail.com>
2018-06-23 20:17:27 +0300

    scroll to comment

--- app/assets/javascripts/favorite.js
+++ app/assets/javascripts/favorite.js
@@ -7,7 +7,15 @@ $(document).ready(function () {
 
   $(document).on('click', '.comment-feedback-link', function (e) {
     e.preventDefault();
-    $('#'+$(this).data('comment')).slideDown(300);
+    $('#' + $(this).data('comment')).slideDown(300);
     $(this).slideUp(300);
+  });
+
+  $('.submit').click(function (e) {
+    e.preventDefault();
+    var _form = $(this).parent();
+    var _action = _form.attr('action');
+    var _id = $(this).data('target') || '#' + _form.attr('id');
+    $(this).parent().attr('action', _action + _id).submit();
   })
 });
